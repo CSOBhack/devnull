2a606ee9de1b50aa2b3557bbcad1d66659aa7f5b
David Cesal <david@cesal.cz>
2015-03-21 11:44:56 +0100

    Initial commit

---
+++ java/src/main/java/cz/csob/hackathon/devnull/Start.java
@@ -0,0 +1,13 @@
+package cz.csob.hackathon.devnull;
+
+/**
+ * Hello world!
+ *
+ */
+public class Start 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
